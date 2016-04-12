3fa01218a0986d6b352241cdb76467ae334f923e
cmc12v <cmc12v@my.fsu.edu>
2016-04-12 16:09:04 +0000

    Lesson 7 Final commit

--- Lesson7_Assignment/drill4.js
+++ Lesson7_Assignment/drill4.js
@@ -6,7 +6,7 @@ Author:Cray Clark
 
 Date:04/12/16
 
-Filename: script.js
+Filename: drill4.js
 
 */
 
@@ -21,4 +21,33 @@ function display(event) {
 
 //attach event listener to h3 elements to invoke display function when clicked
 
-$("h3").click(display);
+//$("h3").click(display);
+
+//displays and animates the next element after the current target
+
+function display2(event) {
+
+ $(event.currentTarget).next().animate( {height: ‘toggle’}, “slow”);
+
+    
+}//end of display2
+
+//attach event listener to h3 elements to invoke display function when clicked
+
+$("h3").click(display2);
+
+//change the background color h3 element when mouse hovers over it
+
+$("h3").hover(function(){
+ 
+ $(this).css("background-color", "red");
+    
+}, function(){
+ 
+ $(this).css("background-color", "purple");
+    
+});
+
+//hover() will trigger display2 method each time mouse hovers over header
+
+$(“h3”).hover(display2);
